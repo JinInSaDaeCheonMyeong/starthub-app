e6723e8f5e263f92e69513f86c3b59b55ad4fe2f
uranny <164876325+uranny@users.noreply.github.com>
2025-09-08 19:49:30 +0900

    fix : shadow at SystemScreen.tsx #76

--- screens/system/SystemScreen.tsx
+++ screens/system/SystemScreen.tsx
@@ -1,20 +1,15 @@
 import { Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import BackButton from "../../component/BackButton";
 import RightArrow from "../../assets/icons/right-arrow-back.svg"
-import AnalysisIcon from '../../assets/icons/analysis.svg';
-import ScaleIcon from '../../assets/icons/scale.svg';
-import CrownIcon from '../../assets/icons/crown.svg';
 import BookmarkIcon from '../../assets/icons/bookMark/bookmark.svg'
 import TimeIcon from '../../assets/icons/section/time.svg'
 import WriteIcon from '../../assets/icons/section/writing.svg'
-import CompanyIcon from '../../assets/icons/section/company.svg'
 import InfoIcon from '../../assets/icons/section/information.svg'
 import ServiceIcon from '../../assets/icons/section/service.svg'
 import { Colors } from "../../constants/Color";
 import { StackScreenProps } from "@react-navigation/stack";
-import { Shadow } from "react-native-shadow-2";
 import { Fonts } from "../../constants/Fonts";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback,useState } from "react";
 import { ShowToast, ToastType } from "../../util/ShowToast";
 import {  isAxiosError } from "axios";
 import { getMe } from "../../api/user";
@@ -41,12 +36,6 @@ export default function SystemScreen({navigation} : SystemScreenProps) {
     const onPress = () => {
         ShowToast("개발", "아직 개발 중인 기능입니다", ToastType.INFO)
     }
-
-    const menus = [
-        {label : '경쟁사\n분석', icon : <AnalysisIcon width={40} height={40} color={Colors.primary} />},
-        {label : '지원금\n비교', icon : <ScaleIcon width={40} height={40} color={Colors.primary} />},
-        {label : '프리미엄\n결제', icon : <CrownIcon width={40} height={40} color={Colors.primary} />}
-    ];
     const sections = [
         {
             title : '나의 활동',
@@ -54,12 +43,6 @@ export default function SystemScreen({navigation} : SystemScreenProps) {
                 {label : '최근 본 게시물', icon : <TimeIcon width={20} height={20} color={Colors.black2}/>},
                 {label : '내 북마크', icon : <BookmarkIcon width={20} height={20} color={Colors.black2}/>},
                 {label : '내 작성글', icon : <WriteIcon width={20} height={20} color={Colors.black2}/>}
-            ]
-        },
-        {
-            title : '나의 기업',
-            sections : [
-                {label : '내 소속', icon : <CompanyIcon width={20} height={20} color={Colors.black2}/>},
             ]
         },
         {
@@ -83,7 +66,6 @@ export default function SystemScreen({navigation} : SystemScreenProps) {
                     }
                 }
             }
-
             fetchUserProfile()
         }, [])
     )
@@ -107,69 +89,37 @@ export default function SystemScreen({navigation} : SystemScreenProps) {
                 contentContainerStyle={styles.scrollContent}
             >
                 <TouchableOpacity onPress={() => navigation.navigate("Profile", {...user})}>
-                    <Shadow 
-                        distance={4} 
-                        offset={[0, 4]}
-                        startColor="rgba(185, 185, 185, 0.2)"
-                        style={styles.profileShadow}
-                    >
-                        <View style={styles.profileCardInner}>
-                            <Image source={{uri: user.profileImage}} style={styles.profileImage}/>
-                                <View style={styles.profileInfo}>
-                                    <Text style={styles.profileCompany}>{companyName}</Text>
-                                    <Text style={styles.profileName}>{user.username}</Text>
-                                </View>
-                            <RightArrow width={16} height={16} color={Colors.black2}/>
-                        </View>
-                    </Shadow>
-                </TouchableOpacity>
-                <View style={styles.menuRowWrap}>
-                    {menus.map(({label, icon}, index) => (
-                        <TouchableOpacity  
-                            onPress={() => onPress()}
-                            key={index} 
-                            style={styles.menuButton}
-                        >
-                            <Shadow
-                                distance={4} 
-                                offset={[0, 4]} 
-                                startColor="rgba(185, 185, 185, 0.2)" 
-                                style={styles.sectionShadow}
-                            >
-                                <View style={styles.menuButtonInner}>
-                                    {icon}
-                                    <Text style={styles.menuButtonText}>{label}</Text>
-                                </View>
-                            </Shadow>
-                        </TouchableOpacity>
-                    ))}
-                </View>
-                {sections.map((value, index) => ( 
-                    <Shadow 
-                        key={index}
-                        distance={4} 
-                        offset={[0, 4]} 
-                        startColor="rgba(185, 185, 185, 0.2)" 
-                        style={styles.sectionShadow}
-                    >
-                        <View style={styles.sectionCard}>
-                            <Text style={styles.sectionTitle}>{value.title}</Text>
-                            <View style={styles.sectionRowWrap}>
-                                {value.sections.map(({label, icon}, idx) => (
-                                    <TouchableOpacity onPress={() => onPress()} key={idx} style={styles.sectionRow}>
-                                        <View style={styles.sectionRowLeft}>
-                                            {icon}
-                                            <Text style={styles.sectionRowText}>{label}</Text>
-                                        </View>
-                                        <RightArrow width={16} height={16} color={Colors.black2}/>
-                                    </TouchableOpacity>
-                                ))}
+                    <View style={styles.profileCardInner}>
+                        <Image source={{uri: user.profileImage}} style={styles.profileImage}/>
+                            <View style={styles.profileInfo}>
+                                <Text style={styles.profileCompany}>{companyName}</Text>
+                                <Text style={styles.profileName}>{user.username}</Text>
                             </View>
+                        <RightArrow width={16} height={16} color={Colors.black2}/>
+                    </View>
+                </TouchableOpacity>
+                {sections.map((value, index) => ( 
+                    <View key={index} style={styles.sectionCard}>
+                        <Text style={styles.sectionTitle}>{value.title}</Text>
+                        <View style={styles.sectionRowWrap}>
+                            {value.sections.map(({label, icon}, idx) => (
+                                <TouchableOpacity onPress={() => onPress()} key={idx} style={styles.sectionRow}>
+                                    <View style={styles.sectionRowLeft}>
+                                        {icon}
+                                        <Text style={styles.sectionRowText}>{label}</Text>
+                                    </View>
+                                    <RightArrow width={16} height={16} color={Colors.black2}/>
+                                </TouchableOpacity>
+                            ))}
                         </View>
-                    </Shadow>
+                    </View>
                 ))}
                 <TouchableOpacity 
-                    style={{alignItems : "flex-end", margin : 12}} 
+                    hitSlop={16}
+                    style={{
+                        alignItems : "flex-end", 
+                        marginRight : 12
+                    }} 
                     onPress={ async() => {
                         try {
                             await removeTokens()
@@ -229,6 +179,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         backgroundColor: Colors.white1,
+        borderWidth : 2,
+        borderColor : Colors.white2,
         borderRadius: 12,
         padding: 16,
         gap: 16,
@@ -270,7 +222,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: 8,
-        paddingVertical : 16
+        paddingVertical : 16,
+        borderWidth : 2,
+        borderColor : Colors.white2,
     },
     menuButtonText: {
         fontFamily: Fonts.semiBold,
@@ -291,6 +245,8 @@ const styles = StyleSheet.create({
     sectionCard: {
         width : "100%",
         backgroundColor: Colors.white1,
+        borderWidth : 2,
+        borderColor : Colors.white2,
         borderRadius: 12,
         padding: 16,
         gap: 16,
