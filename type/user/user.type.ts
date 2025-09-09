1b806a959daa0b83adae395f298d0c3cd85d5f2d
uranny <164876325+uranny@users.noreply.github.com>
2025-09-09 20:05:12 +0900

    fix : change GetMeResponse interface param #78

--- type/user/user.type.ts
+++ type/user/user.type.ts
@@ -1,14 +1,13 @@
+import StartupType from "../../constants/StartupType";
 import { Response } from "../util/response.type";
 
 export interface GetMeResponse extends Response {
     data : {
         id : number
-        email : string
         username : string
         birth : string
         gender : string
-        profileImage : string,
-        introduction : string
+        startupType : StartupType
     }
 }
 
