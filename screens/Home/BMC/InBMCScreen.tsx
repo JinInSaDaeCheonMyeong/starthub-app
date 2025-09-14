6c878d2ceb9f84a650d013c9688d637888bd64b6
김도영 <binj1246@gmail.com>
2025-09-14 18:42:22 +0900

    feat : add in bmc #83

--- screens/Home/BMC/InBMCScreen.tsx
+++ screens/Home/BMC/InBMCScreen.tsx
@@ -1,112 +1,152 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
-    StyleSheet,
-    Dimensions,
-    SafeAreaView,
-    TouchableOpacity,
+    StyleSheet, ScrollView, Dimensions, TouchableOpacity,
 } from 'react-native';
-import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
 import BackButton from '../../../component/BackButton';
 import { Colors } from '../../../constants/Color';
 import { Fonts } from '../../../constants/Fonts';
 import {StackScreenProps} from "@react-navigation/stack";
 import {RootStackParamList} from "../../../navigation/RootStack";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {BMCType, SelectBMCValue} from "../../../type/BMC/BMC.type";
 
-const { width, height } = Dimensions.get('window');
+import ChannelsIcon from "../../../assets/icons/bmc/channels.svg"
+import CostStructureIcon from "../../../assets/icons/bmc/cost_structure.svg"
+import CustomerRelationshipsIcon from "../../../assets/icons/bmc/customer_relationships.svg"
+import CustomerSegmentsIcon from "../../../assets/icons/bmc/customer_segments.svg"
+import KeyActivitiesIcon from "../../../assets/icons/bmc/key_activities.svg"
+import KeyPartnersIcon from "../../../assets/icons/bmc/key_partners.svg"
+import KeyResourcesIcon from "../../../assets/icons/bmc/key_resources.svg"
+import RevenueStreamsIcon from "../../../assets/icons/bmc/revenue_streams.svg"
+import ValuePropositionIcon from "../../../assets/icons/bmc/value_proposition.svg"
 
-const CANVAS_WIDTH = width * 0.95;
-const CANVAS_HEIGHT = height * 0.35;
+const {width} = Dimensions.get('window');
 
-const BMCBlock = ({ title, description, style }: { title: string; description: string; style: any }) => (
-    <View style={[styles.block, style]}>
-        <Text style={styles.title}>{title}</Text>
-        <Text style={styles.desc}>{description}</Text>
-    </View>
-);
 
 type InBMCScreenProps = StackScreenProps<RootStackParamList, 'InBMC'>;
+
+const values = [
+    {
+        itemValue: SelectBMCValue.keyPartners,
+        icon: KeyPartnersIcon,
+    },
+    {
+        itemValue: SelectBMCValue.keyActivities,
+        icon: KeyActivitiesIcon
+    },
+    {
+        itemValue: SelectBMCValue.valueProposition,
+        icon: ValuePropositionIcon,
+    },
+    {
+        itemValue: SelectBMCValue.customerRelationships,
+        icon: CustomerRelationshipsIcon,
+    },
+    {
+        itemValue: SelectBMCValue.customerSegments,
+        icon: CustomerSegmentsIcon,
+    },
+    {
+        itemValue: SelectBMCValue.keyResources,
+        icon: KeyResourcesIcon
+    },
+    {
+        itemValue: SelectBMCValue.channels,
+        icon: ChannelsIcon,
+    },
+    {
+        itemValue: SelectBMCValue.costStructure,
+        icon: CostStructureIcon,
+    },
+    {
+        itemValue: SelectBMCValue.revenueStreams,
+        icon: RevenueStreamsIcon,
+    }
+]
 
 export default function InBMCScreen({navigation, route : {params}} : InBMCScreenProps) {
     const handleBackPress = () => {
         navigation.goBack()
     };
-
+    const buttonSide = (width-96)/5
+    const [selectValue, setSelectValue] = useState<SelectBMCValue>(SelectBMCValue.keyPartners);
+    const insets = useSafeAreaInsets();
+    const bmcBlockMap: Record<SelectBMCValue, string> = {
+        [SelectBMCValue.keyPartners]: params.BMC.keyPartners,
+        [SelectBMCValue.keyActivities]: params.BMC.keyActivities,
+        [SelectBMCValue.valueProposition]: params.BMC.valueProposition,
+        [SelectBMCValue.customerRelationships]: params.BMC.customerRelationships,
+        [SelectBMCValue.customerSegments]: params.BMC.customerSegments,
+        [SelectBMCValue.keyResources]: params.BMC.keyResources,
+        [SelectBMCValue.channels]: params.BMC.channels,
+        [SelectBMCValue.costStructure]: params.BMC.costStructure,
+        [SelectBMCValue.revenueStreams]: params.BMC.revenueStreams,
+    };
     return (
-        <SafeAreaView style={styles.container}>
-            {/* 헤더 */}
+        <View style={[styles.container,{paddingTop: insets.top, paddingBottom: insets.bottom}]}>
             <View style={styles.header}>
                 <BackButton
-                        width={24} 
-                        height={24} 
-                        color={Colors.black2} 
+                        width={24}
+                        height={24}
+                        color={Colors.black2}
                         onClick={handleBackPress}
                 />
                 <Text style={styles.headerTitle}>{params.BMC.title}</Text>
-                <View style={styles.placeholder} />
+                <View style={{paddingEnd: 24}}/>
             </View>
-            
-            {/* BMC 캔버스 */}
-            <ReactNativeZoomableView
-                maxZoom={3}
-                minZoom={0.5}
-                zoomStep={0.5}
-                initialZoom={1}
-                bindToBorders={true}
-                style={[styles.wrapper]}
-            >
-                <View style={styles.centeringView}>
-                    <View style={styles.canvas}>
-                        <BMCBlock 
-                            title="핵심 파트너" 
-                            description={params.BMC.keyPartners}
-                            style={styles.keyPartners} 
-                        />
-                        <BMCBlock 
-                            title="핵심 활동" 
-                            description={params.BMC.keyActivities}
-                            style={styles.keyActivities} 
-                        />
-                        <BMCBlock 
-                            title="핵심 자원" 
-                            description={params.BMC.keyResources}
-                            style={styles.keyResources} 
-                        />
-                        <BMCBlock 
-                            title="가치 제안" 
-                            description={params.BMC.valuePropositions}
-                            style={styles.valuePropositions} 
-                        />
-                        <BMCBlock 
-                            title="고객 관계" 
-                            description={params.BMC.customerRelationships}
-                            style={styles.customerRelationships} 
-                        />
-                        <BMCBlock 
-                            title="채널" 
-                            description={params.BMC.channels}
-                            style={styles.channels} 
-                        />
-                        <BMCBlock 
-                            title="고객 세그먼트" 
-                            description={params.BMC.customerSegments}
-                            style={styles.customerSegments} 
-                        />
-                        <BMCBlock 
-                            title="비용 구조" 
-                            description={params.BMC.costStructure}
-                            style={styles.costStructure} 
-                        />
-                        <BMCBlock 
-                            title="수익 흐름" 
-                            description={params.BMC.revenueStreams}
-                            style={styles.revenueStreams} 
-                        />
+            <ScrollView>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{flexWrap: 'wrap', flexDirection: 'row', paddingStart: 16, paddingTop: 20, paddingBottom: 24}}>
+                        {values.map((value, index) => {
+                            const Icon = value.icon;
+                            const isSelected = value.itemValue === selectValue;
+                            const backGroundColor = isSelected ? Colors.primary : Colors.white1;
+                            const iconColor = isSelected ? Colors.white1 : Colors.primary;
+
+                            return (
+                                <TouchableOpacity key={index} onPress={() => setSelectValue(value.itemValue)}>
+                                    <View style={{
+                                        width: buttonSide,
+                                        height: buttonSide,
+                                        backgroundColor: backGroundColor,
+                                        marginEnd: 16,
+                                        marginBottom: 16,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        borderColor: Colors.primary,
+                                        borderWidth: 2,
+                                        borderRadius: 8
+                                    }}>
+                                        <Icon color={iconColor} width={buttonSide*0.4} height={buttonSide*0.4} />
+                                    </View>
+                                </TouchableOpacity>
+                            );
+                        })}
                     </View>
+                    <View style={{height: 2, width: width-32, paddingHorizontal: 16, backgroundColor: Colors.white2}}/>
+                    <View style={{width: width - 32, alignItems: 'flex-start'}}>
+                        <Text style={{
+                            paddingTop: 24,
+                            paddingBottom: 12,
+                            fontSize: 18,
+                            fontFamily: Fonts.semiBold,
+                            color: Colors.black1,
+                        }}>
+                            {selectValue}
+                        </Text>
+                        <Text style={{
+                            fontSize: 16,
+                            fontFamily: Fonts.medium,
+                        }}>
+                                {bmcBlockMap[selectValue]}
+                        </Text>
+                    </View>
+
                 </View>
-            </ReactNativeZoomableView>
-        </SafeAreaView>
+            </ScrollView>
+        </View>
     );
 }
 
@@ -126,6 +166,7 @@ const styles = StyleSheet.create({
         borderBottomColor: Colors.gray3,
     },
     backButton: {
+        width: 40,
         padding: 8,
     },
     headerTitle: {
@@ -134,110 +175,7 @@ const styles = StyleSheet.create({
         color: Colors.black2,
         textAlign: 'center',
     },
-    placeholder: {
-        width: 40, // backButton과 같은 너비로 설정하여 중앙 정렬
-    },
-    wrapper: {
-        flex: 1,
-    },
-    centeringView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    },
-    canvas: {
-        width: CANVAS_WIDTH,
-        height: CANVAS_HEIGHT,
-        backgroundColor: '#fff',
-        position: 'relative',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 0,
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        elevation: 5,
-    },
-    block: {
-        position: 'absolute',
-        borderWidth: 0.5,
-        borderColor: '#ccc',
-        backgroundColor: '#fafafa',
-        padding: 6,
-        borderRadius: 0,
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 8,
-        marginBottom: 4,
-        color: '#2c3e50',
-        textAlign: 'center',
-    },
-    desc: {
-        fontSize: 5,
-        color: '#555',
-        lineHeight: 10,
-    },
-
-    // BMC 위치 스타일
-    keyPartners: {
-        left: '0%',
-        top: '0%',
-        width: '20%',
-        height: '60%',
-    },
-    keyActivities: {
-        left: '20%',
-        top: '0%',
-        width: '20%',
-        height: '30%',
-    },
-    keyResources: {
-        left: '20%',
-        top: '30%',
-        width: '20%',
-        height: '30%',
-    },
-    valuePropositions: {
-        left: '40%',
-        top: '0%',
-        width: '20%',
-        height: '60%',
-    },
-    customerRelationships: {
-        left: '60%',
-        top: '0%',
-        width: '20%',
-        height: '30%',
-    },
-    channels: {
-        left: '60%',
-        top: '30%',
-        width: '20%',
-        height: '30%',
-    },
-    customerSegments: {
-        left: '80%',
-        top: '0%',
-        width: '20%',
-        height: '60%',
-    },
-    costStructure: {
-        left: '0%',
-        top: '60%',
-        width: '50%',
-        height: '40%',
-    },
-    revenueStreams: {
-        left: '50%',
-        top: '60%',
-        width: '50%',
-        height: '40%',
-    },
+    headerBackPadding: {
+        paddingEnd: 56
+    }
 });
