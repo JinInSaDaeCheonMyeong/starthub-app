265fb2bff210d0463f94d63974fd4357c8a1c515
김도영 <binj1246@gmail.com>
2025-09-14 18:41:59 +0900

    chore : edit bmc type #83

--- type/BMC/BMC.type.ts
+++ type/BMC/BMC.type.ts
@@ -1,16 +1,35 @@
 import {ImageSourcePropType} from "react-native";
+import {Response} from "../util/response.type";
 
 export interface BMCType {
-    title: string;
-    date: Date;
-    thumbnail: ImageSourcePropType;
-    keyPartners: string;
-    keyActivities: string;
-    keyResources: string;
-    valuePropositions: string;
-    customerRelationships: string;
-    channels: string;
-    customerSegments: string;
-    costStructure: string;
-    revenueStreams: string;
+    "id": number,
+    "title": string,
+    "keyPartners": string,
+    "keyActivities": string,
+    "keyResources": string,
+    "valueProposition": string,
+    "customerRelationships": string,
+    "channels": string,
+    "customerSegments": string,
+    "costStructure": string,
+    "revenueStreams": string,
+    "isCompleted": boolean,
+    "createdAt": string,
+    "updatedAt": string,
+}
+
+export interface GetBMCsResponse extends Response{
+    data : BMCType[];
+}
+
+export enum SelectBMCValue {
+    keyPartners = "keyPartners",
+    keyActivities = "keyActivities",
+    keyResources = "keyResources",
+    valueProposition = "valueProposition",
+    customerRelationships = "customerRelationships",
+    channels = "channels",
+    customerSegments = "customerSegments",
+    costStructure = "costStructure",
+    revenueStreams = "revenueStreams",
 }
