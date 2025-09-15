1ad375c71f62252e9b5ae2e7bdcaac2beb4d200e
김도영 <binj1246@gmail.com>
2025-09-15 20:32:44 +0900

    feat : create notice type

--- type/notice/notice.type.ts
+++ type/notice/notice.type.ts
@@ -1,14 +1,31 @@
-import { NoticeCategory } from "../../constants/NoticeCategory"
+import {Response} from "../util/response.type";
 
-export interface NoticeItemType {
-    id : number
-    category : NoticeCategory
-    title : string
-    startTime : Date
-    endTime : Date
-    location : string
-    target  : string
-    years : string[]
-    entre : string
-    webLink : string
+export interface NoticeTypeTrailer {
+    content: NoticeType[],
+    page: number,
+    size: number,
+    totalPages: number,
+    totalElements: number,
+    isLast: boolean,
+}
+
+export interface GetNoticesResponse extends Response{
+    data : NoticeTypeTrailer[];
+}
+
+export interface NoticeType {
+    id: number,
+    title: string,
+    url: string,
+    organization: string,
+    receptionPeriod: number,
+    likeCount: number,
+    supportField: string,
+    targetAge: string,
+    contactNumber: string,
+    region: string,
+    organizationType: string,
+    startupHistory: string,
+    departmentInCharge: string,
+    content: string
 }
