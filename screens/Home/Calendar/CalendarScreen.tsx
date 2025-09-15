b1ead66a4ac352dd525f2b83d172193e9893e4e8
uranny <164876325+uranny@users.noreply.github.com>
2025-09-15 15:33:01 +0900

    fix : add custom calendar header #89

--- screens/Home/Calendar/CalendarScreen.tsx
+++ screens/Home/Calendar/CalendarScreen.tsx
@@ -2,32 +2,30 @@ import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import {Calendar, LocaleConfig} from 'react-native-calendars';
 import { Colors } from "../../../constants/Color";
 import { Fonts } from "../../../constants/Fonts";
-
-LocaleConfig.locales['ko'] = {
-    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-    monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-    dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-    dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-    today: '오늘'
-};
+import { formatToDate } from "../../../util/DateFormat";
+import LeftIcon from "../../../assets/icons/left-arrow-back.svg"
+import RightIcon from "../../../assets/icons/right-arrow-back.svg"
 
 LocaleConfig.defaultLocale = 'ko';
 
 export default function CalendarScreen() {
+    const dotInfoList = [
+        { color: Colors.info, text: "마감 4주전" },
+        { color: Colors.warning, text: "마감 2주전" },
+        { color: Colors.error, text: "마감 1주전" },
+    ];
+    const dayDataList = ['일', '월', '화', '수', '목', '금', '토']
+
     return (
-        <ScrollView style={{padding : 16}}>
+        <ScrollView 
+            showsVerticalScrollIndicator={false}
+            style={{padding : 16}}
+        >
             <Calendar
-                style={{gap : 12,}}
+                style={{gap : 8, marginBottom : 24}}
                 theme={{
                     calendarBackground : Colors.white1,
                     weekVerticalMargin : 0,
-                    stylesheet : {
-                        calendar : {
-                            header : {
-                                undefined
-                            }
-                        }
-                    },
                 }}
                 onDayPress={(day) => {console.log(day)}}
                 markingType={"multi-dot"}
@@ -42,6 +40,80 @@ export default function CalendarScreen() {
                         selected: true,
                         selectedColor: "blue",
                     },
+                }}
+                customHeader={(props : any) => {
+                    return (
+                        <View style={{
+                            gap : 8
+                        }}>
+                            <View style={{
+                                justifyContent : 'space-between',
+                                alignItems : 'center',
+                                flexDirection : 'row'
+                            }}>
+                                <Text style={{
+                                    fontFamily : Fonts.semiBold,
+                                    fontSize : 20
+                                }}>
+                                    {formatToDate(props.month, "calendar")}
+                                </Text>
+                                <View style={{
+                                    flexDirection : 'row',
+                                    gap : 16
+                                }}>
+                                    <TouchableOpacity hitSlop={8} onPress={() => props.addMonth(-1)}>
+                                        <LeftIcon width={20} height={20} color={Colors.black2}/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity hitSlop={8} onPress={() => props.addMonth(1)}>
+                                        <RightIcon width={20} height={20} color={Colors.black2}/>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <View style={{
+                                flexDirection : 'row',
+                                gap : 8
+                            }}>
+                                {dotInfoList.map((value, index) => (
+                                    <View style={{flexDirection : 'row', alignItems : 'center', gap : 6, padding : 4}} key={index}>
+                                        <View style={{
+                                            width : 10,
+                                            height : 10,
+                                            borderRadius : 5,
+                                            backgroundColor : value.color
+                                        }}/>
+                                        <Text style={{
+                                            fontFamily : Fonts.medium,
+                                            fontSize : 14, 
+                                            color : Colors.black2
+                                        }}>
+                                            {value.text}
+                                        </Text>
+                                    </View>
+                                ))}
+                            </View>
+                            <View style={{
+                                flexDirection : 'row',
+                                paddingVertical : 6,
+                                marginTop : 16
+                            }}>
+                                {dayDataList.map((value, index) => (
+                                    <View key={index} 
+                                    style={{
+                                        flex : 1
+                                    }}>
+                                    <Text style={{
+                                        color : index === 0 ? Colors.error : index === 6 ? Colors.info : Colors.black2,
+                                        fontSize : 14,
+                                        fontFamily : Fonts.medium,
+                                        textAlign : 'center'
+                                    }}>
+                                            {value}
+                                        </Text>
+                                    </View>
+                                ))}
+                            </View>
+                        </View>
+                    );
                 }}
                 dayComponent={({date, state, marking, onPress}) => {
                     return (
