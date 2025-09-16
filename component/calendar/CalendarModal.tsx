abb05263f4ee1bb4b1a391ff34f65ef4d493d10a
uranny <164876325+uranny@users.noreply.github.com>
2025-09-17 08:52:35 +0900

    chore : divide tag and style and add blank text #89

--- component/calendar/CalendarModal.tsx
+++ component/calendar/CalendarModal.tsx
@@ -1,6 +1,6 @@
 import { BottomSheetBackdrop, BottomSheetFlashList, BottomSheetFlatList, BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet"
 import React, { useCallback, useMemo, useState } from "react";
-import { FlatList, Text, TouchableOpacity, useWindowDimensions, View } from "react-native"
+import { FlatList, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from "react-native"
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import XIcon from "../../assets/icons/xmark.svg"
 import { Colors } from "../../constants/Color";
@@ -30,16 +30,8 @@ export default function CalendarModal({
     const listMaxHeight = currentSnapHeight - headerHeight - 32 - insets.top - insets.bottom;
     return(            
         <BottomSheetModal
-            handleStyle={{
-                borderRadius : 16,
-                backgroundColor : Colors.white1,
-                paddingVertical : 16
-            }}
-            handleIndicatorStyle={{
-                width : 60,
-                height : 4,
-                backgroundColor : Colors.gray3,
-            }}
+            handleStyle={styles.handleStyle}
+            handleIndicatorStyle={styles.handleIndicator}
             ref={bottomSheetModalRef}
             snapPoints={snapPoints}
             topInset={insets.top}
@@ -55,55 +47,43 @@ export default function CalendarModal({
             )}
             onChange={(index) => setCurrentSnapIndex(index)}
         >
-            <BottomSheetView style={{
-                    flex : 1,
-                    paddingTop : insets.top,
-                    paddingBottom : insets.bottom,
-                    position: 'relative',
-                    borderRadius : 16
-                }}>
-                    <FlatList
-                        showsVerticalScrollIndicator={false}
-                        contentContainerStyle={{
-                            gap : 16,
-                            padding : 16,
-                        }}
-                        style={{ 
-                            maxHeight: listMaxHeight, 
-                            overflow : 'visible'
-                        }}
-                        keyExtractor={(item : any) => item.id.toString()}
-                        keyboardShouldPersistTaps="handled"
-                        data={scheduleList}
-                        renderItem={({item} : any) => (
-                            <NoticeItem 
-                                {...item}
-                                isHome={false}
-                                onPress={() => {console.log(`${item.target}`)}}
-                            />
-                        )}
-                    />
-                    <View style={{
-                            position : 'absolute',
-                            flexDirection : 'row', 
-                            alignItems : 'center', 
-                            justifyContent : 'space-between',
-                            width : "100%",
-                            paddingHorizontal : 16,
-                            paddingTop : 8,
-                            paddingBottom : 24,
-                            borderBottomWidth : 2,
-                            borderColor : Colors.white2,
-                            backgroundColor : Colors.white1
-                        }}
+            <BottomSheetView style={[
+                    styles.bottomSheetView, 
+                    {
+                        paddingTop : insets.top,
+                        paddingBottom : insets.bottom,
+                    }
+                ]}>
+                    { scheduleList.length !== 0 ? (
+                        <FlatList
+                            showsVerticalScrollIndicator={false}
+                            contentContainerStyle={styles.contentContainer}
+                            style={[styles.flatList, { 
+                                maxHeight: listMaxHeight,
+                            }]}
+                            keyExtractor={(item : NoticeItemType) => item.id.toString()}
+                            keyboardShouldPersistTaps="handled"
+                            data={scheduleList}
+                            renderItem={({item}) => (
+                                <NoticeItem
+                                    {...item}
+                                    isHome={false}
+                                    onPress={() => {console.log(item.webLink)}}
+                                />
+                            )}
+                        />
+                    ) : (
+                        <View style={styles.errorMsgBox}>
+                            <Text style={styles.errorText}>
+                                {`해당 날짜에 일정이 없습니다`}
+                            </Text>
+                        </View>
+                    )}
+                    <View style={styles.headerContainer}
                         onLayout={(e) => setHeaderHeight(e.nativeEvent.layout.height)}
                     >
-                        <View style={{width : 16, height : 16}}/>
-                        <Text style={{
-                            color : Colors.black1,
-                            fontFamily : Fonts.medium,
-                            fontSize : 14
-                        }}>
+                        <View style={styles.blankBox}/>
+                        <Text style={styles.bottomSheetTitleText}>
                             {day}
                         </Text>
                         <TouchableOpacity onPress={handleModalClose} hitSlop={16}>
@@ -114,3 +94,60 @@ export default function CalendarModal({
         </BottomSheetModal>
     )
 }
+
+const styles = StyleSheet.create({
+    handleStyle : {
+        borderRadius : 16,
+        backgroundColor : Colors.white1,
+        paddingVertical : 16
+    },
+    handleIndicator : {
+        width : 60,
+        height : 4,
+        backgroundColor : Colors.gray3,
+    },
+    bottomSheetView : {
+        flex : 1,
+        position: 'relative',
+        borderRadius : 16
+    },
+    contentContainer : {
+        gap : 16,
+        padding : 16,
+    },
+    flatList : { 
+        overflow : 'visible'
+    },
+    headerContainer : {
+        position : 'absolute',
+        flexDirection : 'row', 
+        alignItems : 'center', 
+        justifyContent : 'space-between',
+        width : "100%",
+        paddingHorizontal : 16,
+        paddingTop : 8,
+        paddingBottom : 24,
+        borderBottomWidth : 2,
+        borderColor : Colors.white2,
+        backgroundColor : Colors.white1
+    },
+    blankBox : {
+        width : 16, 
+        height : 16
+    },
+    bottomSheetTitleText : {
+        color : Colors.black1,
+        fontFamily : Fonts.medium,
+        fontSize : 14
+    },
+    errorMsgBox : {
+        justifyContent : 'center',
+        alignItems : 'center',
+        margin : 32
+    },
+    errorText : {
+        fontSize : 16,
+        fontFamily : Fonts.semiBold,
+        color : Colors.error
+    }
+})
