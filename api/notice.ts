2ccc8e642147fc3da086bad6a71edd24af942bdf
김도영 <binj1246@gmail.com>
2025-09-17 09:25:34 +0900

    chore : Change to filterable API

--- api/notice.ts
+++ api/notice.ts
@@ -1,9 +1,22 @@
 import StartHubAxios from "../lib/StartHubAxios";
 import {GetNoticesResponse} from "../type/notice/notice.type";
 
-export const getNotices = async (page : number) : Promise<GetNoticesResponse> =>
-    (await StartHubAxios.get('announcements', {
+export const getNotices = async (
+    title : string,
+    supportField : string,
+    targetRegion : string,
+    targetAge : string,
+    businessExperience : string,
+    page : number
+) : Promise<GetNoticesResponse> =>
+    (await StartHubAxios.get('announcements/search', {
         params : {
+            title : title,
+            supportField : supportField,
+            targetRegion : targetRegion,
+            targetAge : targetAge,
+            businessExperience : businessExperience,
+            includeLikeStatus : true,
             page : page,
             size : 15
         }
