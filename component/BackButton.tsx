1015b9e21f2cc2b6908f462390204b984ff91584
김도영 <binj1246@gmail.com>
2025-09-29 20:45:15 +0900

    fix : fix back button #120

--- component/BackButton.tsx
+++ component/BackButton.tsx
@@ -1,14 +1,31 @@
-import LeftArrow from '../assets/icons/left-arrow-back.svg';
+import { useState } from "react";
+import LeftArrow from "../assets/icons/left-arrow-back.svg";
+import {TouchableOpacity} from "react-native";
 
 type BackButtonProps = {
-    width : number
-    height : number
-    color : string
-    onClick : () => void
-}
+    width: number;
+    height: number;
+    color: string;
+    onClick: () => void;
+};
 
-export default function BackButton(props : BackButtonProps){
+export default function BackButton(props: BackButtonProps) {
+    const [disabled, setDisabled] = useState(false);
+
+    const handleClick = () => {
+        if (disabled) return; // 이미 눌린 상태면 무시
+        setDisabled(true);
+        props.onClick();
+        setTimeout(() => setDisabled(false), 500); // 0.5초 후 다시 활성화
+    };
+
     return (
-    <LeftArrow width={props.width} height={props.height} hitSlop={16} color={props.color} onTouchEnd={props.onClick}/>
-)
+        <TouchableOpacity onPress={handleClick} hitSlop={16}>
+        <LeftArrow
+            width={props.width}
+            height={props.height}
+            color={props.color}
+        />
+        </TouchableOpacity>
+    );
 }
