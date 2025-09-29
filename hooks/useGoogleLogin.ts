d3f239fe88b99981005366202e95f4231d3e6d50
김도영 <binj1246@gmail.com>
2025-09-29 19:55:33 +0900

    fix : android google login

--- hooks/useGoogleLogin.ts
+++ hooks/useGoogleLogin.ts
@@ -1,54 +1,81 @@
-import * as WebBrowser from 'expo-web-browser';
-import {makeRedirectUri, useAuthRequest, ResponseType} from 'expo-auth-session';
-import { useEffect } from 'react';
-import {Platform} from "react-native";
-import {googleLogin} from "../api/oauth";
-import {ShowToast, ToastType} from "../util/ShowToast";
+import * as WebBrowser from "expo-web-browser";
+import {makeRedirectUri, ResponseType, useAuthRequest} from "expo-auth-session";
+import * as Google from "expo-auth-session/providers/google";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+import { googleLogin } from "../api/oauth";
+import { ShowToast, ToastType } from "../util/ShowToast";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const discovery = {
-    authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
 };
 
-export default function useGoogleLogin(
-    onSuccess?: (isFirst: boolean) => void
-) {
-    const redirectUri = makeRedirectUri({scheme: 'com.jininsa.startHubapp',});
-    const clientId = Platform.select({
-        ios: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_IOS,
-        android: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID
-    })
-    const [request, response, promptAsync] = useAuthRequest(
-        {
-            clientId: clientId,
-            redirectUri,
-            responseType: ResponseType.Code,
-            scopes: ['openid', 'profile', 'email'],
-        },
-        discovery
-    );
+export default function useGoogleLogin(onSuccess?: (isFirst: boolean) => void) {
+  const androidClientIdFull =
+      process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID || "";
+  const androidClientIdSub = androidClientIdFull.replace(
+      ".apps.googleusercontent.com",
+      ""
+  );
+  const clientId = Platform.select({
+    ios: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_IOS,
+    android: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID
+  })
+  const nativeRedirect = Platform.select({
+    android: androidClientIdSub
+        ? `com.googleusercontent.apps.${androidClientIdSub}:/oauthredirect`
+        : "startHub-app://redirect",
+  });
+  const scheme = Platform.select({
+    android: "startHub-app",
+    ios : "com.jininsa.startHubapp",
+  })
 
-    useEffect(() => {
-        const handleResponse = async () => {
-            if (response?.type === 'success' && response.params?.code && request?.codeVerifier) {
-                const isFirst = await googleLogin(response.params.code, request?.codeVerifier);
-                if (onSuccess && typeof isFirst !== 'undefined') {
-                    onSuccess(isFirst);
-                }
-            } else if (response?.type === 'error') {
-                ShowToast(
-                    "문제가 발생하였습니다.",
-                    response.params.error,
-                    ToastType.ERROR,
-                )
+  const redirectUri = makeRedirectUri({
+    scheme: scheme,
+    native: nativeRedirect,
+  });
+
+  const [request, response, promptAsync] = useAuthRequest({
+    clientId,
+    responseType: ResponseType.Code,
+    scopes: ["openid", "profile", "email"],
+    usePKCE: true,
+    redirectUri,
+  },
+  discovery
+  );
+
+  useEffect(() => {
+    const handleResponse = async () => {
+      try {
+        if (response?.type === "success") {
+          const code = (response as any)?.params?.code;
+          const codeVerifier = request?.codeVerifier;
+          if (code && codeVerifier) {
+            console.log("Code Verifier: ",codeVerifier);
+            console.log("Code: ",code);
+            const isFirst = await googleLogin(code, codeVerifier);
+            if (onSuccess && typeof isFirst !== "undefined") {
+              onSuccess(isFirst);
             }
-        };
-        handleResponse();
-    }, [response]);
-
-    return {
-        request,
-        promptAsync,
+          }
+        } else if (response?.type === "error") {
+          const err = (response as any)?.params?.error || "구글 로그인 오류";
+          ShowToast("문제가 발생하였습니다.", err, ToastType.ERROR);
+        }
+      } catch (e: any) {
+        ShowToast(
+            "문제가 발생하였습니다.",
+            e?.message || String(e),
+            ToastType.ERROR
+        );
+      }
     };
+    handleResponse();
+  }, [response]);
+
+  return { request, promptAsync };
 }
--- screens/StartScreen.tsx
+++ screens/StartScreen.tsx
@@ -1,171 +1,162 @@
-import {StackScreenProps} from "@react-navigation/stack";
-import {AuthStackParamList} from "../navigation/AuthStack";
-import {RootStackParamList} from "../navigation/RootStack";
-import {CompositeScreenProps} from "@react-navigation/native";
+import { StackScreenProps } from "@react-navigation/stack";
+import { AuthStackParamList } from "../navigation/AuthStack";
+import { RootStackParamList } from "../navigation/RootStack";
+import { CompositeScreenProps } from "@react-navigation/native";
 import {
-    Image,
-    Platform,
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View
+  Image,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from "react-native";
-import {Colors} from "../constants/Color";
+import { Colors } from "../constants/Color";
 import React from "react";
 import BackButton from "../component/BackButton";
 import useGoogleLogin from "../hooks/useGoogleLogin";
-import {Fonts} from "../constants/Fonts";
-import {ShowToast, ToastType} from "../util/ShowToast";
-
+import { Fonts } from "../constants/Fonts";
+import { ShowToast, ToastType } from "../util/ShowToast";
 
 type StartScreenProps = CompositeScreenProps<
-    StackScreenProps<AuthStackParamList, 'Start'>,
-    StackScreenProps<RootStackParamList>
+  StackScreenProps<AuthStackParamList, "Start">,
+  StackScreenProps<RootStackParamList>
 >;
 
-export default function StartScreen({navigation}: StartScreenProps) {
-    const platform = Platform.select({
-        ios: "ios",
-        android: "android"
-    })
-    const { request, promptAsync } = useGoogleLogin( isFirst => {
-        console.log("isFirst", isFirst);
-            if (isFirst){
-                navigation.navigate("SignupInput")
-            }
-            else {
-                navigation.navigate('HomeStack');
-            }
-        }
-    );
+export default function StartScreen({ navigation }: StartScreenProps) {
+  const platform = Platform.select({
+    ios: "ios",
+    android: "android",
+  });
+  const { request, promptAsync } = useGoogleLogin((isFirst) => {
+    console.log("isFirst", isFirst);
+    if (isFirst) {
+      navigation.navigate("SignupInput");
+    } else {
+      navigation.navigate("HomeStack");
+    }
+  });
 
-    const authItems = [
-        {
-            title: 'StartHub 이메일',
-            icon: require('../assets/logos/starthub_logo.png'),
-            onPress: async () => {
-                await navigation.navigate("Signin")
-            },
-            request : true
-        },
-        {
-            title: 'Google',
-            icon: require('../assets/logos/google_logo.png'),
-            onPress: async () => {
-                if (platform == "ios") {
-                    await promptAsync();
-                }
-                else {
-                    ShowToast(
-                        "안내",
-                        "아직 개발 중인 기능입니다.",
-                        ToastType.INFO
-                    )
-                }
-            },
-            request: request
-        },
-        {
-            title: '네이버',
-            icon: require('../assets/logos/naver_logo.png'),
-            onPress: async () => {
-                ShowToast(
-                    "안내",
-                    "아직 개발 중인 기능입니다.",
-                    ToastType.INFO
-                )
-            },
-            request: true
-        },
-        {
-            title: 'Apple',
-            icon: require('../assets/logos/apple_logo.png'),
-            onPress: async () => {
-                ShowToast(
-                    "안내",
-                    "아직 개발 중인 기능입니다.",
-                    ToastType.INFO
-                )
-            },
-            request: true
-        }
-    ];
-    return (
-        <View style={styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.backButton}>
-                    <BackButton onClick={() => navigation.goBack()} width={20} height={20} color={Colors.black2}/>
-                </View>
-                <Text style={styles.topText}>
-                    안녕하세요!{"\n"}계정을{" "}
-                    <Text style={{
-                        color: Colors.primary}}>
-                        선택
-                    </Text>
-                    해주세요.
-                </Text>
-                <View style={styles.itemContainer}>
-                    {
-                        authItems.map((item, index) => (
-                            <TouchableOpacity style={styles.authItem} key={index} onPress={item.onPress} disabled={!item.request}>
-                                <Image source={item.icon} style={styles.itemIcon}/>
-                                <Text style={styles.itemText}>{item.title}로 시작하기</Text>
-                            </TouchableOpacity>
-                        ))
-                    }
-                </View>
-                <StatusBar barStyle="dark-content" backgroundColor={Colors.white1}/>
-            </ScrollView>
+  const authItems = [
+    {
+      title: "StartHub 이메일",
+      icon: require("../assets/logos/starthub_logo.png"),
+      onPress: async () => {
+        await navigation.navigate("Signin");
+      },
+      request: true,
+    },
+    {
+      title: "Google",
+      icon: require("../assets/logos/google_logo.png"),
+      onPress: async () => {
+        await promptAsync();
+      },
+      request: request,
+    },
+    {
+      title: "네이버",
+      icon: require("../assets/logos/naver_logo.png"),
+      onPress: async () => {
+        ShowToast("안내", "아직 개발 중인 기능입니다.", ToastType.INFO);
+      },
+      request: true,
+    },
+    {
+      title: "Apple",
+      icon: require("../assets/logos/apple_logo.png"),
+      onPress: async () => {
+        ShowToast("안내", "아직 개발 중인 기능입니다.", ToastType.INFO);
+      },
+      request: true,
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.backButton}>
+          <BackButton
+            onClick={() => navigation.goBack()}
+            width={20}
+            height={20}
+            color={Colors.black2}
+          />
         </View>
-    );
+        <Text style={styles.topText}>
+          안녕하세요!{"\n"}계정을{" "}
+          <Text
+            style={{
+              color: Colors.primary,
+            }}
+          >
+            선택
+          </Text>
+          해주세요.
+        </Text>
+        <View style={styles.itemContainer}>
+          {authItems.map((item, index) => (
+            <TouchableOpacity
+              style={styles.authItem}
+              key={index}
+              onPress={item.onPress}
+              disabled={!item.request}
+            >
+              <Image source={item.icon} style={styles.itemIcon} />
+              <Text style={styles.itemText}>{item.title}로 시작하기</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <StatusBar barStyle="dark-content" backgroundColor={Colors.white1} />
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        margin : 16
-    },
-    backButton: {
-        marginTop: 22,
-    },
-    topText: {
-        marginTop: 74,
-        alignItems: 'center',
-        fontSize: 24,
-        fontFamily : Fonts.bold,
-        textAlign: 'center',
-        color: Colors.black2,
-    },
-    authItem: {
-        height: 60,
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderColor: Colors.gray3,
-        borderRadius: 8,
-        marginBottom: 16,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-    itemContainer: {
-        marginTop: 68,
-    },
-    itemIcon: {
-        width: 20,
-        height: 20,
-        marginLeft: 22,
-        resizeMode: 'contain'
-    },
-    itemText: {
-        flex: 1,
-        fontSize: 16,
-        fontFamily : Fonts.medium,
-        textAlign: 'center',
-        color: Colors.black2,
-        marginRight: 42,
-    },
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 16,
+  },
+  backButton: {
+    marginTop: 22,
+  },
+  topText: {
+    marginTop: 74,
+    alignItems: "center",
+    fontSize: 24,
+    fontFamily: Fonts.bold,
+    textAlign: "center",
+    color: Colors.black2,
+  },
+  authItem: {
+    height: 60,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderColor: Colors.gray3,
+    borderRadius: 8,
+    marginBottom: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  itemContainer: {
+    marginTop: 68,
+  },
+  itemIcon: {
+    width: 20,
+    height: 20,
+    marginLeft: 22,
+    resizeMode: "contain",
+  },
+  itemText: {
+    flex: 1,
+    fontSize: 16,
+    fontFamily: Fonts.medium,
+    textAlign: "center",
+    color: Colors.black2,
+    marginRight: 42,
+  },
+});
