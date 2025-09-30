7000b2f08e23ad4c5e0eb8973cae93880a7063fe
uranny <164876325+uranny@users.noreply.github.com>
2025-09-30 11:52:44 +0900

    feat : add api function of delete user #127

--- api/user.ts
+++ api/user.ts
@@ -1,10 +1,8 @@
 import { SigninRequest, SigninResponse } from "../type/user/signin.type";
 import { SignupRequest } from "../type/user/signup.type";
-import { RefreshRequest, RefreshResponse } from "../type/user/refresh.type";
 import StartHubAxios from "../lib/StartHubAxios";
 import { Response } from "../type/util/response.type";
 import { GetMeResponse, GetUserResponse } from "../type/user/user.type";
-import { getAccToken } from "../util/token";
 import { SetProfileRequest } from "../type/user/profile.type";
 
 const GET_TOKEN_API_HEADERS = {'X-Platform': 'app'}
@@ -23,7 +21,10 @@ export const setProfile = async (setProfileData: SetProfileRequest): Promise<Res
     (await StartHubAxios.patch('/user/profile', setProfileData)).data;
 
 export const getMe = async () : Promise<GetMeResponse> => 
-    (await StartHubAxios.get('/user/me', {headers : {Authorization : await getAccToken()}})).data
+    (await StartHubAxios.get('/user/me')).data
 
 export const getUser = async (userId : number) : Promise<GetUserResponse> => 
-    (await StartHubAxios.get(`/user/${userId}/profile`, {headers : {Authorization : await getAccToken()}})).data
+    (await StartHubAxios.get(`/user/${userId}/profile`)).data
+
+export const deleteUser = async(deleteUserData : {password : string}) : Promise<Response> => 
+    (await StartHubAxios.delete('/user/delete', {data : deleteUserData})).data
