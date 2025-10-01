669374027f5168945cd101ced6416e215d6a8e41
uranny <164876325+uranny@users.noreply.github.com>
2025-10-01 17:32:48 +0900

    refactor : refactor StartHubAxios #150

--- lib/StartHubAxios.ts
+++ lib/StartHubAxios.ts
@@ -1,13 +1,21 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import { getAccToken, getRefToken, removeTokens, saveAccToken, saveRefToken } from "../util/token";
 import { BackHandler, Platform } from "react-native";
 import { RefreshResponse } from "../type/user/refresh.type";
+import { ShowToast, ToastType } from "../util/ShowToast";
+import popToSigninScreen from "../util/NavigationService";
+
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+    _retry ?: boolean
+}
 
 const StartHubAxios = axios.create({
     baseURL: process.env.EXPO_PUBLIC_API_URL,
     timeout: 5000,
 });
 
+// 요청
 StartHubAxios.interceptors.request.use(
     async (config) => {
         const token = await getAccToken();
@@ -19,37 +27,39 @@ StartHubAxios.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
+// 응답
 StartHubAxios.interceptors.response.use(
     (response) => response,
     async (error: AxiosError) => {
-        const originalRequest: any = error.config;
+        const originalRequest = error.config as CustomAxiosRequestConfig;
 
         if (
-            error.response?.status === 401 &&
-            !originalRequest._retry &&
-            !originalRequest.url.includes("/user/reissue")
+            error.response?.status === 401 && 
+            originalRequest &&
+            !originalRequest._retry
         ) {
             originalRequest._retry = true;
+
             try {
-                const refreshData = await getRefToken();
-                if (!refreshData) throw new Error("No refresh token");
-                const {
-                    data: { access: accessToken, refresh: refreshToken },
-                }: RefreshResponse = (await axios.post(
+                const refresh = await getRefToken();
+                if (!refresh) throw new Error("리프레시 토큰이 없습니다");
+
+                const response : RefreshResponse = (await axios.post(
                     `${process.env.EXPO_PUBLIC_API_URL}user/reissue`,
-                    { refresh : refreshData },
+                    { refresh },
                     {headers: {'X-Platform': 'app'}}
                 )).data
-                await saveAccToken(accessToken);
-                await saveRefToken(refreshToken);
-                originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+                if (!response.data.access) throw new Error("토큰을 받아오지 못하였습니다");
+
+                await saveAccToken(response.data.access);
+                await saveRefToken(response.data.refresh);
+
+                originalRequest.headers.Authorization = `Bearer ${response.data.access}`;
                 return StartHubAxios(originalRequest);
             } catch (error) {
                 await removeTokens();
-                Platform.select({
-                    ios: () => console.log("exit"),
-                    android: () => BackHandler.exitApp()
-                });
+                popToSigninScreen()
                 return Promise.reject(error);
             }
         }
