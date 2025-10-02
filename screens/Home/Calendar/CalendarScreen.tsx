0a9413720cf72cb9e944aa44d13366fefb81bdea
uranny <164876325+uranny@users.noreply.github.com>
2025-10-03 01:02:20 +0900

    fix : fix design CalendarScreen.tsx #164

--- screens/Home/Calendar/CalendarScreen.tsx
+++ screens/Home/Calendar/CalendarScreen.tsx
@@ -1,11 +1,10 @@
-import { ScrollView, Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View, StyleSheet,useWindowDimensions, FlatList } from "react-native";
 import { Calendar, LocaleConfig } from 'react-native-calendars';
 import { Colors } from "../../../constants/Color";
 import { Fonts } from "../../../constants/Fonts";
 import { formatToDate } from "../../../util/DateFormat";
 import LeftIcon from "../../../assets/icons/left-arrow-back.svg";
 import RightIcon from "../../../assets/icons/right-arrow-back.svg";
-import CalendarModal from "../../../component/calendar/CalendarModal";
 import * as Progress from "react-native-progress";
 import useCalendarScreen from "../../../hooks/home/useCalendarScreen";
 import { ShowToast, ToastType } from "../../../util/ShowToast";
@@ -14,7 +13,11 @@ import { HomeStackParamList } from "../../../navigation/HomeStack";
 import { RootStackParamList } from "../../../navigation/RootStack";
 import { StackScreenProps } from "@react-navigation/stack";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import GlassView from "../../../component/GlassView";
+import { NoticeType } from "../../../type/notice/notice.type";
+import NoticeItem from "../../../component/notice/NoticeItem";
+import XIcon from "../../../assets/icons/xmark.svg";
 
 LocaleConfig.locales['ko'] = {
     monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
@@ -45,166 +48,169 @@ export default function CalendarScreen({navigation} : CalendarScreenProps) {
             dotInfoList,
             dayDataList,
             markedDates,
-            bottomSheetModalRef
         },
-        action : {
-            handleModalClose,
-            handleModalOpen
-        }
     } = useCalendarScreen()
+    const {width} = useWindowDimensions()
     const today = new Date()
-    const [currentMonth, setCurrentMonth] = useState(today.getMonth() + 1)
-
+    const todayString = formatToDate(today, 'solid');
+    const [currentDate, setCurrentDate] = useState(todayString)
+    const [viewingMonth, setViewingMonth] = useState(todayString.substring(0, 7))
     return (
         <>
-        <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-            <Calendar
-                onMonthChange={(date) => {
-                    console.log(date.month)
-                    setCurrentMonth(date.month)
-                }}
-                style={styles.calendar}
-                theme={{
-                    calendarBackground: Colors.white1,
-                    weekVerticalMargin: 0,
-                }}
-                markingType={"multi-dot"}
-                markedDates={markedDates}
-                customHeader={(props: any) => {
-                    return (
-                        <View style={styles.headerContainer}>
-                            <View style={styles.headerTop}>
-                                <Text style={styles.headerTitle}>
-                                    {formatToDate(props.month.toDateString(), 'calendar')}
-                                </Text>
-                                <View style={styles.headerButtons}>
-                                    <TouchableOpacity hitSlop={8} onPress={() => props.addMonth(-1)}>
-                                        <LeftIcon width={20} height={20} color={Colors.black2} />
+        <View style={styles.container}>
+            <GlassView
+                containerStyle={{padding : 20}}
+            >
+                <Calendar
+                    style={styles.calendar}
+                    theme={{
+                        weekVerticalMargin : 0,
+                        calendarBackground: 'transparent',
+                        backgroundColor: 'transparent',
+                    }}
+                    onMonthChange={(date) => {
+                        setViewingMonth(date.dateString.substring(0, 7))
+                    }}
+                    markingType={"multi-dot"}
+                    markedDates={markedDates}
+                    customHeader={(props: any) => {
+                        const currentMonth = formatToDate(props.month, 'calendar')
+                    
+                        return (
+                            <View style={styles.headerContainer}>
+                                <View style={styles.headerTop}>
+                                    <TouchableOpacity
+                                        hitSlop={8}
+                                        onPress={() => {
+                                        props.addMonth(-1);
+                                        const prevMonth = new Date(props.month.getFullYear(), props.month.getMonth() - 1, 1);
+                                        setViewingMonth(formatToDate(prevMonth, 'solid').substring(0, 7));
+                                        }}
+                                    >
+                                        <LeftIcon width={16} height={16} color={Colors.black2} />
                                     </TouchableOpacity>
-                                    <TouchableOpacity hitSlop={8} onPress={() => props.addMonth(1)}>
-                                        <RightIcon width={20} height={20} color={Colors.black2} />
-                                    </TouchableOpacity>
+                            
+                                    <Text style={styles.headerTitle}>
+                                        {currentMonth}
+                                    </Text>
+                        
+                                    <View style={styles.headerButtons}>
+                                        <TouchableOpacity
+                                        hitSlop={8}
+                                        onPress={() => {
+                                            props.addMonth(1);
+                                            const nextMonth = new Date(props.month.getFullYear(), props.month.getMonth() + 1, 1);
+                                            setViewingMonth(formatToDate(nextMonth, 'solid').substring(0, 7));
+                                        }}
+                                        >
+                                        <RightIcon width={16} height={16} color={Colors.black2} />
+                                        </TouchableOpacity>
+                                    </View>
                                 </View>
                             </View>
-                            <View style={styles.dotLegend}>
-                                {dotInfoList.map((value, index) => (
-                                    <View style={styles.dotLegendItem} key={index}>
-                                        <View style={[styles.dotLegendDot, { backgroundColor: value.color }]} />
-                                        <Text style={styles.dotLegendText}>{value.text}</Text>
-                                    </View>
-                                ))}
-                            </View>
-                            <View style={styles.weekDays}>
-                                {dayDataList.map((value, index) => (
-                                    <View key={index} style={styles.weekDayItem}>
-                                        <Text
-                                            style={[
-                                                styles.weekDayText,
-                                                index === 0 && { color: Colors.error },
-                                                index === 6 && { color: Colors.info },
-                                            ]}
-                                        >
-                                            {value}
-                                        </Text>
-                                    </View>
-                                ))}
-                            </View>
-                        </View>
-                    )
-                }}
-                dayComponent={({date, state, marking, onPress}) => {
-                    const dotIds = marking?.dots?.map((dot : any) => dot.id) ?? [];
-                    const isDisabled = state === 'disabled';
-                    const isToday = state === 'today'
-                    const isSameMonth = date?.month === currentMonth;
-                    console.log(`${date?.dateString} : ${date?.month } ${today.getMonth() + 1}`)
-                    console.log(`${date?.dateString} : ${isDisabled} ${isToday} ${isSameMonth}`)
-                    const backgroundColor = isDisabled ? Colors.white2 : 
-                                            isToday ? 
-                                            isSameMonth ? Colors.white1 : Colors.white2 : Colors.white1
-                    return (
-                        <TouchableOpacity
-                            key={date?.dateString ?? 'undefind'}
-                            style={[
-                                styles.dayContainer,
-                                { backgroundColor },
-                            ]}
-                            onPress={async () => {
-                                try {
-                                    onPress?.(date);
-                                    setLoading(true);
-                                    const result = await getNoticeItem(dotIds);
-                                    setNoticeItemList(result)
-                                    setDay(
-                                        `${date ? `${date.month}월 ${date.day}일 공고 일정` : "날짜를 찾을 수 없습니다"}`
-                                    );
-                                    setLoading(false);
-                                    handleModalOpen();
-                                } catch (error) {
-                                    ShowToast("오류 발생", "일정 리스트를 불러 올 수 없습니다", ToastType.ERROR)
-                                }
-                            }}
-                        >
-                            <View
+                        )
+                    }}
+                    dayComponent={({date, state, marking, onPress}) => {
+                        const dotIds = marking?.dots?.map((dot : any) => dot.id) ?? [];
+                        const isSelect = date?.dateString === currentDate;
+                        const isDisable = state === 'disabled'
+                        const isToday = date?.dateString === todayString 
+                        const dateMonth = date?.dateString?.substring(0, 7)
+                        
+                        const shouldHideToday = isToday && dateMonth !== viewingMonth
+
+                        return isDisable || !shouldHideToday && (
+                            <TouchableOpacity
+                                key={date?.dateString ?? 'undefind'}
                                 style={[
-                                    styles.dayCircle,
-                                    { 
-                                        borderRadius: 16,
-                                        backgroundColor: state === 'today' ? Colors.primary : undefined 
-                                    },
+                                    styles.dayContainer,
+                                    {
+                                        width : (width - 36)/7,
+                                        backgroundColor : isSelect ? Colors.primary : 'transparent'
+                                    }
                                 ]}
+                                onPress={async () => {
+                                    try {
+                                        onPress?.(date);
+                                        setCurrentDate(date?.dateString ?? currentDate)
+                                        setLoading(true);
+                                        const result = await getNoticeItem(dotIds);
+                                        setNoticeItemList(result)
+                                        setDay(
+                                            `${date ? `${date.month}월 ${date.day}일 공고 일정` : "날짜를 찾을 수 없습니다"}`
+                                        );
+                                        setLoading(false);
+                                    } catch (error) {
+                                        ShowToast("오류 발생", "일정 리스트를 불러 올 수 없습니다", ToastType.ERROR)
+                                    }
+                                }}
                             >
                                 <Text
                                     style={[
                                         styles.dayText,
-                                        state === 'today'
+                                        isSelect
                                             ? { color: Colors.white1 }
-                                            : state !== 'disabled'
+                                            : !isDisable
                                             ? { color: Colors.black2 }
                                             : { color: Colors.gray3 },
                                     ]}
                                 >
                                     {date?.day}
                                 </Text>
-                            </View>
-                            {marking?.dots && (
-                                <View style={styles.dotsContainer}>
-                                    {marking.dots.slice(0,3).map((dot, index) => (
-                                        <View
-                                            key={index}
-                                            style={[styles.dot, { backgroundColor: dot.color }]}
-                                        />
-                                    ))}
-                                </View>
-                            )}
-                        </TouchableOpacity>
-                    )
+                                {marking?.dots && !isSelect && (
+                                    <View style={styles.dotsContainer}>
+                                        {marking.dots.slice(0,3).map((dot, index) => (
+                                            <View
+                                                key={index}
+                                                style={[styles.dot, { backgroundColor: dot.color }]}
+                                            />
+                                        ))}
+                                    </View>
+                                )}
+                            </TouchableOpacity>
+                        )
+                    }}
+                />
+            </GlassView>
+            <FlatList
+                bounces={false}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingTop : 8,
+                    gap : 12,
                 }}
+                style={{
+                    flex: 1,  
+                }}
+                keyExtractor={(item : NoticeType) => item.id.toString()}
+                keyboardShouldPersistTaps="handled"
+                data={noticeItemList}
+                renderItem={({item}) => (
+                    <NoticeItem
+                        item={item}
+                        isHome={false}
+                        onPress={() => {navigation.navigate("InNotice", { Notice : item });}}
+                    />
+                )}
+                ListEmptyComponent={() => (
+                    <View style={styles.errorMsgBox}>
+                        <XIcon color={Colors.error} width={32} height={32}/>
+                        <Text style={styles.errorText}>
+                            {`해당 날짜에 일정이 없습니다`}
+                        </Text>
+                    </View>
+                )}
             />
-            <CalendarModal
-                scheduleList={noticeItemList}
-                day={day}
-                bottomSheetModalRef={bottomSheetModalRef}
-                handleModalClose={() => {
-                    handleModalClose()
-                }}
-                onNoticeItemPress={(Notice) => {
-                    handleModalClose();
-                    navigation.navigate('InNotice', {Notice})
-                }}
-            />
-        </ScrollView>
+        </View>
         {loading && (
             <View style={styles.progressContainer}>
-                <View>
                 <Progress.Circle
-                    color={Colors.primary}
+                    color={Colors.second}
                     size={50}
                     indeterminate = {true}
                     thickness = {300}
                     borderWidth={4}
                 />
-                </View>
             </View>
         )}
         </>
@@ -213,12 +219,13 @@ export default function CalendarScreen({navigation} : CalendarScreenProps) {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         padding: 16,
-        position : 'relative'
+        position : 'relative',
+        gap : 8
     },
     calendar: {
-        gap: 8,
-        marginBottom: 24,
+        gap: 9,
     },
     headerContainer: {
         gap: 8,
@@ -230,7 +237,7 @@ const styles = StyleSheet.create({
     },
     headerTitle: {
         fontFamily: Fonts.semiBold,
-        fontSize: 20,
+        fontSize: 16,
     },
     headerButtons: {
         flexDirection: 'row',
@@ -256,46 +263,33 @@ const styles = StyleSheet.create({
         fontSize: 14,
         color: Colors.black2,
     },
-    weekDays: {
-        flexDirection: 'row',
-        paddingVertical: 6,
-        marginTop: 16,
-    },
-    weekDayItem: {
-        flex: 1,
-    },
-    weekDayText: {
-        fontSize: 14,
-        fontFamily: Fonts.medium,
-        textAlign: 'center',
-        color: Colors.black2,
-    },
     dayContainer: {
         alignItems: 'center',
-        paddingHorizontal: 10,
-        height: 90,
+        paddingVertical : 5,
+        borderRadius : 16,
+        margin : 9,
+        maxWidth : 30,
+        gap : 1
     },
     dayCircle: {
-        width: 32,
-        height: 32,
+        padding : 4,
         borderRadius: 16,
         alignItems: 'center',
         justifyContent: 'center',
     },
     dayText: {
-        fontFamily: Fonts.medium,
-        fontSize: 14,
+        fontFamily: Fonts.semiBold,
+        fontSize: 16,
     },
     dotsContainer: {
         flexDirection: 'row',
-        marginTop: 6,
         alignItems: 'center',
         justifyContent: 'center',
         gap: 4,
     },
     dot: {
-        width: 6,
-        height: 6,
+        width: 4,
+        height: 4,
         borderRadius: 3,
     },
     progressContainer : {
@@ -304,7 +298,18 @@ const styles = StyleSheet.create({
         height : '100%',
         alignItems : 'center',
         justifyContent : "center",
-        backgroundColor : Colors.black1,
-        opacity : 0.5
+        backgroundColor : "rgba(0, 0, 0, 0.6)",
+    },
+    errorMsgBox : {
+        justifyContent : 'center',
+        alignItems : 'center',
+        marginTop : 32,
+        marginBottom : 48,
+        gap : 24
+    },
+    errorText : {
+        fontSize : 16,
+        fontFamily : Fonts.semiBold,
+        color : Colors.error
     }
 });
