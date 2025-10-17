82796080c3cdaa84f6f2b1ef8044bcd48f98c8a2
김도영 <binj1246@gmail.com>
2025-10-17 11:24:56 +0900

    refactor : remove unused #197

--- api/company.ts
+++
@@ -1,5 +0,0 @@
-import StartHubAxios from "../lib/StartHubAxios";
-import { GetCompanyByIdResponse } from "../type/company/company.type";
-
-export const getCompanyById = async (id : number) : Promise<GetCompanyByIdResponse> => 
-    (await StartHubAxios.get(`/company/${id}`)).data
