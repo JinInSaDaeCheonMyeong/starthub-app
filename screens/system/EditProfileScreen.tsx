90d7ec67c07a36794a9f6932d4706846f989f1f5
uranny <164876325+uranny@users.noreply.github.com>
2025-10-20 16:50:01 +0900

    fix : fix design of EditProfileScreen #235

--- screens/system/EditProfileScreen.tsx
+++ screens/system/EditProfileScreen.tsx
@@ -5,6 +5,16 @@ import { Colors } from "../../constants/Color";
 import { Fonts } from "../../constants/Fonts";
 import useEditProfileScreen from "../../hooks/system/useEditProfileScreen";
 import SubHeaderBar from "../../component/home/SubHeaderBar";
+import NameIcon from "../../assets/icons/profile/name.svg";
+import GenderIcon from "../../assets/icons/profile/gender.svg";
+import BirthIcon from "../../assets/icons/profile/birth.svg";
+import BackpackIcon from "../../assets/icons/profile/backpack.svg";
+import CompanyIcon from "../../assets/icons/profile/company.svg";
+import IntroduceIcon from "../../assets/icons/profile/introduce.svg";
+import PeopleIcon from "../../assets/icons/profile/people.svg";
+import LocationIcon from "../../assets/icons/profile/location.svg";
+import SiteIcon from "../../assets/icons/profile/link.svg";
+import MoneyIcon from "../../assets/icons/profile/money.svg";
 
 export type ProfileScreenProps = StackScreenProps<SystemStackParamList, 'EditProfile'>
 
@@ -61,7 +71,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                     paddingBottom : 16
             }}>
                 <View style={styles.dataInputContainer}>
-                    <Text style={styles.titleText}>이름</Text>
+                    <View style={styles.iconBox}>
+                        <NameIcon width={15} height={15}/>
+                        <Text style={styles.titleText}>이름</Text>
+                    </View>
                     <TextInput
                         style={styles.dataInputText}
                         value={user.username} 
@@ -71,32 +84,47 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                     />
                 </View>
                 <View style={styles.dataInputContainer}>
-                    <Text style={styles.titleText}>성별</Text>
+                    <View style={styles.iconBox}>
+                        <GenderIcon width={15} height={15}/>
+                        <Text style={styles.titleText}>성별</Text>
+                    </View>
                     <View style={styles.genderContainer}>
                         <TouchableOpacity
                             onPress={() => setSelectGender("male")}
-                            style={[styles.genderBox, {borderColor : selectGender === "male" ? Colors.primary : Colors.white2 }]}
+                            style={[styles.genderBox, {
+                                borderColor : selectGender === "male" ? Colors.primary : Colors.gray3,
+                                backgroundColor : selectGender === "male" ? 'rgba(36,102,244, 0.1)' : Colors.white1
+                            }]}
                         >
                             <Text style={[styles.selectText, {color : selectGender === "male" ? Colors.primary : Colors.gray2 }]}>남</Text>
                         </TouchableOpacity>
 
                         <TouchableOpacity
                             onPress={() => setSelectGender("female")}
-                            style={[styles.genderBox, {borderColor : selectGender === "female" ? Colors.primary : Colors.white2 }]}
+                            style={[styles.genderBox, {
+                                borderColor : selectGender === "female" ? Colors.primary : Colors.gray3,
+                                backgroundColor : selectGender === "female" ? 'rgba(36,102,244, 0.1)' : Colors.white1
+                            }]}
                         >
                             <Text style={[styles.selectText, {color : selectGender === "female" ? Colors.primary : Colors.gray2 }]}>여</Text>
                         </TouchableOpacity>
 
                         <TouchableOpacity
                             onPress={() => setSelectGender("other")}
-                            style={[styles.genderBox, {borderColor : selectGender === "other" ? Colors.primary : Colors.white2 }]}
+                            style={[styles.genderBox, {
+                                borderColor : selectGender === "other" ? Colors.primary : Colors.gray3,
+                                backgroundColor : selectGender === "other" ? 'rgba(36,102,244, 0.1)' : Colors.white1
+                            }]}
                         >
                             <Text style={[styles.selectText, {color : selectGender === "other" ? Colors.primary : Colors.gray2 }]}>선택 안함</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
                 <View style={styles.dataInputContainer}>
-                    <Text style={styles.titleText}>생년월일</Text>
+                    <View style={styles.iconBox}>
+                        <BirthIcon width={15} height={15}/>
+                        <Text style={styles.titleText}>생년월일</Text>
+                    </View>
                     <View style={styles.genderContainer}>
                         <TextInput
                             style={[styles.dataInputText, {flex : 1, textAlign : "center"}]}
@@ -128,17 +156,26 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                     </View>
                 </View>
                 <View style={styles.dataInputContainer}>
-                    <Text style={styles.titleText}>창업 형태</Text>
+                    <View style={styles.iconBox}>
+                        <BackpackIcon width={15} height={15}/>
+                        <Text style={styles.titleText}>창업 형태</Text>
+                    </View>
                     <View style={styles.genderContainer}>
                         <TouchableOpacity 
                             onPress={() => setSelectStartupStatus(true)}
-                            style={[styles.genderBox, {borderColor : selectStartupStatus ? Colors.primary : Colors.white2 }]}
+                            style={[styles.genderBox, {
+                                borderColor : selectStartupStatus ? Colors.primary : Colors.gray3,
+                                backgroundColor : selectStartupStatus ? 'rgba(36,102,244, 0.1)' : Colors.white1
+                            }]}
                         >
                             <Text style={[styles.selectText, {color : selectStartupStatus ? Colors.primary : Colors.gray2 }]}>초기 창업</Text>
                         </TouchableOpacity>
                         <TouchableOpacity 
                             onPress={() => setSelectStartupStatus(false)}
-                            style={[styles.genderBox, {borderColor : !selectStartupStatus ? Colors.primary : Colors.white2 }]}
+                            style={[styles.genderBox, {
+                                borderColor : !selectStartupStatus ? Colors.primary : Colors.gray3,
+                                backgroundColor : !selectStartupStatus ? 'rgba(36,102,244, 0.1)' : Colors.white1
+                            }]}
                         >
                             <Text style={[styles.selectText, {color : !selectStartupStatus ? Colors.primary : Colors.gray2 }]}>예비 창업</Text>
                         </TouchableOpacity>
@@ -148,7 +185,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                 {selectStartupStatus ? (
                     <>
                     <View style={styles.dataInputContainer}>
-                        <Text style={styles.titleText}>기업명</Text>
+                        <View style={styles.iconBox}>
+                            <CompanyIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>기업명</Text>
+                        </View>
                         <TextInput
                             style={styles.dataInputText}
                             value={user.companyName} 
@@ -161,7 +201,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                         />
                     </View>
                     <View style={styles.dataInputContainer}>
-                        <Text style={styles.titleText}>(선택) 기업 소개</Text>
+                        <View style={styles.iconBox}>
+                            <IntroduceIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>기업 소개</Text>
+                        </View>
                         <TextInput
                             style={[styles.dataInputText]}
                             value={user.companyDescription} 
@@ -177,7 +220,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                         />
                     </View>
                     <View style={styles.dataInputContainer}>
-                        <Text style={styles.titleText}>기업 인원</Text>
+                        <View style={styles.iconBox}>
+                            <PeopleIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>기업 인원</Text>
+                        </View>
                         <TextInput
                             style={styles.dataInputText}
                             value={numberPerson} 
@@ -189,8 +235,27 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                             }}
                         />
                     </View>
+                    <View style={[styles.dataInputContainer]}>
+                        <View style={styles.iconBox}>
+                            <LocationIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>기업 위치</Text>
+                        </View>
+                        <TextInput
+                            style={styles.dataInputText}
+                            value={user.startupLocation} 
+                            placeholder="창업 위치를 입력해주세요..."
+                            placeholderTextColor={Colors.gray2}
+                            onChangeText={(value) => {setUser({
+                                ...user, 
+                                startupLocation : value
+                            })}}
+                        />
+                    </View>
                     <View style={styles.dataInputContainer}>
-                        <Text style={styles.titleText}>(선택) 기업 사이트</Text>
+                        <View style={styles.iconBox}>
+                            <SiteIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>기업 사이트</Text>
+                        </View>
                         <TextInput
                             style={styles.dataInputText}
                             value={user.companyWebsite} 
@@ -205,7 +270,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                         />
                     </View>
                     <View style={styles.dataInputContainer}>
-                        <Text style={styles.titleText}>연매출액</Text>
+                        <View style={styles.iconBox}>
+                            <MoneyIcon width={15} height={15}/>
+                            <Text style={styles.titleText}>연매출액</Text>
+                        </View>
                         <TextInput
                             style={styles.dataInputText}
                             value={annualRevenue} 
@@ -215,23 +283,10 @@ export default function EditProfileScreen(props: ProfileScreenProps){
                             onChangeText={(value) => {setAnnualRevenue(value)}}
                         />
                     </View>
-                    <View style={[styles.dataInputContainer]}>
-                        <Text style={styles.titleText}>(선택) 창업 위치</Text>
-                        <TextInput
-                            style={styles.dataInputText}
-                            value={user.startupLocation} 
-                            placeholder="창업 위치를 입력해주세요..."
-                            placeholderTextColor={Colors.gray2}
-                            onChangeText={(value) => {setUser({
-                                ...user, 
-                                startupLocation : value
-                            })}}
-                        />
-                    </View>
                     </>
                 ) : (
                     <View style={[styles.dataInputContainer]}>
-                        <Text style={styles.titleText}>(선택) 창업 위치</Text>
+                        <Text style={styles.titleText}>기업 위치</Text>
                         <TextInput
                             style={styles.dataInputText}
                             value={user.startupLocation ?? ''} 
@@ -296,10 +351,12 @@ const styles = StyleSheet.create({
     },
     dataInputText : {
         fontSize : 14,
-        fontFamily : Fonts.medium,
-        color : Colors.black2,
-        backgroundColor : Colors.white2,
-        borderRadius : 8,
+        fontFamily : Fonts.reqular,
+        color : Colors.black1,
+        backgroundColor : Colors.white1,
+        borderRadius : 10,
+        borderWidth : 1,
+        borderColor : Colors.gray3,
         padding : 16
     },
     genderBox : {
@@ -322,7 +379,12 @@ const styles = StyleSheet.create({
         color : Colors.black2
     },
     line : {
-        borderBottomWidth : 2,
-        borderColor : Colors.white2,
+        borderBottomWidth : 1,
+        borderColor : Colors.gray3,
+    },
+    iconBox : {
+        flexDirection : 'row', 
+        gap : 4, 
+        alignItems  :'center'
     }
 })
